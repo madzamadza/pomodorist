5239b0869c48386293f745350fd1e5fe69df49af
madzamadza <ivana.madzarevic89@gmail.com>
2017-11-30 12:14:15 -0800

    added html draft

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,71 @@
+#
+container {
+    border: 1 px solid #333;
+  border-radius :20px;
+  width:400px;
+  margin:20px auto;
+  padding:20px;
+  text-align:center;
+  background : # 333;
+}
 
+#
+timer {
+    color: #f00;
+    font - size: 50 px;
+    margin: 10 px auto;
+    border: 5 px solid red;
+    border - radius: 50 % ;
+    width: 200 px;
+    height: 200 px;
+    overflow: hidden;
+    position: relative; -
+    webkit - user - select: none; -
+    moz - user - select: none; -
+    ms - user - select: none;
+    user - select: none;
+    cursor: default;
+}
+
+#
+time {
+    margin - top: 70 px;
+    z - index: 1;
+    position: relative;
+}
+
+#
+filler {
+    background: #ddffcc;
+    height: 0 px;
+    width: 200 px;
+    position: absolute;
+    bottom: 0;
+}
+
+#
+buttons button {
+    background: #4da6ff; 
+  border:none; 
+  color:# fff;
+    cursor: pointer;
+    padding: 5 px;
+    width: 90 px;
+    margin: 10 px auto;
+    font - size: 14 px;
+    height: 50 px;
+    border - radius: 50 px;
+}
+
+#
+buttons button# shortBreak {
+    background: #0c0;
+}
+
+# buttons button# longBreak {
+            background: #080;
+}
+
+# buttons button# stop {
+                background: #f00;
+            }
